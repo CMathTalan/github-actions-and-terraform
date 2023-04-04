875ddb2dc13197eb45cc5bda54f992e9fa443299
Mathieu Cailleau <mathieu.cailleau@talan.com>
2023-04-04 15:59:17 +0200

    déclaration des variables

--- az-remote-backend-variables.tf
+++ az-remote-backend-variables.tf
@@ -2,12 +2,14 @@
 variable "company" {
   type = string
   description = "défini le nom de la company"
+  default = "framleueul"
 }
 
 # environment
 variable "environment" {
   type = string
   description = "défini l'environnement à construire"
+  default = "francecentral"
 }
 
 #azure region
