2d6abdc61548d7ceef19214114ce45e538ca9175
Mathieu Cailleau <mathieu.cailleau@talan.com>
2023-04-04 16:13:10 +0200

    correction provider

--- .github/workflows/04-terraform-provider.tf
+++ .github/workflows/04-terraform-provider.tf
@@ -3,18 +3,19 @@ terraform {
   required_version = ">= 1.3"
   backend "azurerm" {
     resource_group_name  = "github-actions-and-terraform-rg"
-    storage_account_name = "kterraform2182"
+    storage_account_name = "terraform2182"
     container_name       = "core-tfstate"
     key                  = "actions.tfstate"
   }
   required_providers {
     azurerm = {
-      version = "~>3.2"
+      version = "=3.2"
       source  = "hashicorp/azurerm"
     }
   }
 }
+
 # Configure the Azure provider
-provider azurerm { 
+provider "azurerm" { 
   features {}  
 }
